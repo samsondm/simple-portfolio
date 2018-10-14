51d09f67f4d88d8c6e32f1e6de999470edf0b938
Dmitry <41114590+samsondm@users.noreply.github.com>
2018-10-14 11:49:52 +0300

    Updates

---
+++ precache-manifest.47d45ef5fd4d426b61713c8181bd0fa4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0706d73a33aa10f23f823c4e79b82cb",
+    "url": "/simple-portfolio/static/media/drum-machine.b0706d73.png"
+  },
+  {
+    "revision": "ff9975762d92be2e3ded",
+    "url": "/simple-portfolio/static/css/main.1f4576f5.chunk.css"
+  },
+  {
+    "revision": "a91b581da1321170f5cc29d17b82644f",
+    "url": "/simple-portfolio/static/media/DroidSansMono-webfont.a91b581d.woff"
+  },
+  {
+    "revision": "e78214f79e47ac796a56",
+    "url": "/simple-portfolio/static/js/1.e78214f7.chunk.js"
+  },
+  {
+    "revision": "fb782bb126f432bb3fd4",
+    "url": "/simple-portfolio/static/js/runtime~main.fb782bb1.js"
+  },
+  {
+    "revision": "b0c0716f271c98c7f6236129a2ee8ebd",
+    "url": "/simple-portfolio/static/media/calc.b0c0716f.png"
+  },
+  {
+    "revision": "ff9975762d92be2e3ded",
+    "url": "/simple-portfolio/static/js/main.ff997576.chunk.js"
+  },
+  {
+    "revision": "7e9338028ec634ee29fd86d26f665e4f",
+    "url": "/simple-portfolio/static/media/pomodoro-clock.7e933802.png"
+  },
+  {
+    "revision": "a66cf96c456729792aeb440cded4a6d4",
+    "url": "/simple-portfolio/static/media/random-quote-machine.a66cf96c.png"
+  },
+  {
+    "revision": "42d2c6ff9bcd4f3bc601a09276fad40e",
+    "url": "/simple-portfolio/static/media/weather-app.42d2c6ff.png"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/simple-portfolio/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "e78214f79e47ac796a56",
+    "url": "/simple-portfolio/static/css/1.b2e668d0.chunk.css"
+  },
+  {
+    "revision": "8a20a06b8421fdef04a18493ebb1d119",
+    "url": "/simple-portfolio/index.html"
+  }
+];
